1bfb4981ede408935f99840aa68c13101dec2307
DmytroMigirov <dmytromigirov@gmail.com>
2024-03-14 11:11:10 +0100

    Added check for zero determinant in the inverse function

--- cells_alg.py
+++ cells_alg.py
@@ -32,6 +32,9 @@ def det(A):
 
 def inverse(A):
     result = []
+    det_A = det(A)
+    if det_A == 0:
+        return None  # Матрица вырожденная, обратной не существует
     for i in range(len(A)):
         result.append([])
         for j in range(len(A[0])):
